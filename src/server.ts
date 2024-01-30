2f4e24187bb10878a215753b742dacab78e25f4c
Paweł gabryel <pawel.gabryel@apps.clinic>
2024-01-30 17:47:19 +0100

    add .env

--- src/server.ts
+++ src/server.ts
@@ -4,7 +4,8 @@ import cors from 'cors';
 import { colorRouter } from './routers/color.router';
 import { userRouter } from './routers/user.router';
 
-const PORT = 3005;
+export const CLIENT_URL = process.env.CLIENT_URL;
+export const PORT = process.env.PORT;
 const app = express();
 
 app.use(cors());
@@ -13,5 +14,5 @@ app.use('/users', userRouter);
 app.use('/colors', colorRouter);
 
 app.listen(PORT, () => {
-	console.log(`API is ready on http://localhost:${PORT} 🚀🚀🚀`);
+	console.log(`API is ready on ${CLIENT_URL}:${PORT} 🚀🚀🚀`);
 });
