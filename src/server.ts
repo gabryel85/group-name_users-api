4933eb82a7fdd09e67ec1dd19ec34b575fac2870
Paweł gabryel <pawel.gabryel@apps.clinic>
2024-01-31 11:56:53 +0100

    env rollback

--- src/server.ts
+++ src/server.ts
@@ -4,8 +4,7 @@ import cors from 'cors';
 import { colorRouter } from './routers/color.router';
 import { userRouter } from './routers/user.router';
 
-export const CLIENT_URL = process.env.CLIENT_URL;
-export const PORT = process.env.PORT;
+const PORT = 3005;
 const app = express();
 
 app.use(cors());
@@ -14,5 +13,5 @@ app.use('/users', userRouter);
 app.use('/colors', colorRouter);
 
 app.listen(PORT, () => {
-	console.log(`API is ready on ${CLIENT_URL}:${PORT} 🚀🚀🚀`);
+	console.log(`API is ready on http://localhost:${PORT} 🚀🚀🚀`);
 });
